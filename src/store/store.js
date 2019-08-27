ae5a35c49a8822a22af25448862044ddd1168da2
Evans Wu <711hikaru@gmail.com>
2019-08-27 21:32:01 +0800

    完成撥放器 vuex 設計

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-import * as types from './mutation-types'
+import Vuex from 'vuex';
+import * as types from './mutation-types';
 
-import createLogger from 'vuex/dist/logger'
+import {playMode} from 'common/js/config';
+
+import createLogger from 'vuex/dist/logger';
 
 Vue.use(Vuex)
 
@@ -10,14 +12,47 @@ const debug = process.env.NODE_ENV !== 'production';
 
 export default new Vuex.Store({
   state: {
-    singer: {}
+    singer: {},
+    playing: false,
+    fullScreen: false,
+    playList: [],
+    sequenceList: [],
+    mode: playMode.sequence,
+    currentIndex: -1
   },
   getters: {
-    singer: state => state.singer
+    singer: state => state.singer,
+    playing: state => state.playing,
+    fullScreen: state => state.fullScreen,
+    playList: state => state.playList,
+    sequenceList: state => state.sequenceList,
+    mode: state => state.mode,
+    currentIndex: state => state.currentIndex,
+    currentSong: (state) => {
+      return state.playList[state.currentIndex] || {}
+    }
   },
   mutations: {
     [types.SET_SINGER](state,singer) {
       state.singer = singer;
+    },
+    [types.SET_PLAYING_STATE](state, flag) {
+      state.playing = flag
+    },
+    [types.SET_FULL_SCREEN](state,flag) {
+      state.fullScreen = flag
+    },
+    [types.SET_PLAYLIST](state,list) {
+      state.playList = list
+    },
+    [types.SET_SEQUENCE_LIST](state,list) {
+      state.sequenceList = list
+    },
+    [types.SET_PLAY_MODE](state, mode) {
+      state.mode = mode
+    },
+    [types.SET_CURRENT_INDEX](state,index) {
+      state.currentIndex = index
     }
   },
   actions: {
