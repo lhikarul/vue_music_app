c95ebb289f441c0102124ddd925fbe852d223c66
lhikarul <711hikaru@gmail.com>
2019-08-31 17:06:38 +0800

    4-3 JSONP 抓取 QQ推薦清單

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -2,7 +2,9 @@ import originJSONP from 'jsonp';
 
 export default function jsonp(url,data,option) {
 
-    url += (url.indexOf('?') == -1 ? '?' : '&' + param(data));
+    url += (url.indexOf('?') == -1 ? '?' : '&') + param(data);
+    
+    // url => https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg?g_tk=5381&inCharset=utf-8&outCharset=utf-8&notice=0&format=jsonp&platform=h5&uin=0&needNewCode=1
 
     return new Promise((resolve,reject) => {
         originJSONP(url,option,(err,data) => {
@@ -23,7 +25,11 @@ function param (data) {
     for (let k in data) {
         let value = data[k] !== undefined ? data[k] : '';
         url += `&${k}=${encodeURIComponent(value)}`;
+
     }
+    
+    // 拼湊出的 query url 
+    // &g_tk=5381&inCharset=utf-8&outCharset=utf-8&notice=0&format=jsonp&platform=h5&uin=0&needNewCode=1
 
     return url ? url.substring(1) : '';
 
--- src/views/recommend/Recommend.vue
+++ src/views/recommend/Recommend.vue
@@ -3,3 +3,21 @@
         recommend
     </div>
 </template>
+
+<script>
+import {getRecommendList} from 'api/recommend';
+
+export default {
+    name: 'Recommend',
+    methods: {
+        requestRecommendList () {
+            getRecommendList().then((res) => {
+                console.log(res.data)
+            })
+        }
+    },
+    created () {
+        this.requestRecommendList();
+    }
+}
+</script>
