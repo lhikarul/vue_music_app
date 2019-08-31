dd8fd4bf0bae50955861e7723a63ecb05fa10bc4
lhikarul <711hikaru@gmail.com>
2019-08-31 15:53:06 +0800

    music app 初始化、 3-1 header css

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <header-component></header-component>
+    </div>
+</template>
+
+<script>
+import HeaderComponent from 'components/header/Header.vue';
+
+export default {
+    name: 'app',
+    components: {
+        HeaderComponent
+    }
+}
+</script>
+
+<style lang="scss">
+
+
+
+</style>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header">
+        <div class="icon"></div>
+        <h1 class="text">Chick Music</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'headerComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '~common/scss/header/header';
+    
+</style>
