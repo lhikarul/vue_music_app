2df4daa5f5811466c2ea068d6d54f372d1a47941
lhikarul <711hikaru@gmail.com>
2019-09-08 15:58:42 +0800

    6-11 css transform 調整滾動

--- src/components/music-list/musicList.vue
+++ src/components/music-list/musicList.vue
@@ -65,7 +65,19 @@ export default {
     watch: {
         scrollY (newY) {
             var translateY = Math.max(this.minTranslateY, newY);
+            var zIndex = 0;
             this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`;
+
+            if (newY < this.minTranslateY) {
+                zIndex = 10;
+                this.$refs.bgImage.style.paddingTop = 0;
+                this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`;
+            }else {
+                this.$refs.bgImage.style.paddingTop = '70%';
+                this.$refs.bgImage.style.height = 0;
+            }
+
+            this.$refs.bgImage.style.zIndex = zIndex;
         }
     },
     created() {
