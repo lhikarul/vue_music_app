d04c38804f0c0648363c8c0172a26c46cf627db8
lhikarul <711hikaru@gmail.com>
2019-09-14 15:56:28 +0800

    player 動畫效果

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,85 +1,96 @@
 <template>
     <div class="player" v-show="playlist.length > 0">
-
-        <div class="normal-player" v-show="fullScreen">
-            <div class="background">
-                <img width="100%" height="100%" :src="currentSong.image">
-            </div>
-
-            <div class="top">
-                <div class="back" @click="back">
-                    <i class="icon-back"></i>
+        
+        <transition name="normal" @enter="enter" @after-enter="afterEnter" @leave="leave" @after-leave="afterLeave">
+        
+            <div class="normal-player" v-show="fullScreen">
+                <div class="background">
+                    <img width="100%" height="100%" :src="currentSong.image">
                 </div>
-                <h1 class="title">{{currentSong.name}}</h1>
-                <h2 class="subtitle">{{currentSong.singer}}</h2>
-            </div>
 
-            <div class="middle">
-                <div class="middle-l">
-                    <div class="cd-wrapper">
-                        <div class="cd">
-                            <img class="image" :src="currentSong.image">
+                <div class="top">
+                    <div class="back" @click="back">
+                        <i class="icon-back"></i>
+                    </div>
+                    <h1 class="title">{{currentSong.name}}</h1>
+                    <h2 class="subtitle">{{currentSong.singer}}</h2>
+                </div>
+
+                <div class="middle">
+                    <div class="middle-l">
+                        <div class="cd-wrapper" ref="cdWrapper">
+                            <div class="cd">
+                                <img class="image" :src="currentSong.image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bottom">
+                    
+                    <div class="progress-wrapper">
+                        <span class="time time-l"></span>
+                        <div class="progress-bar-wrapper">
+                            
+                        </div>
+                        <span class="time time-r"></span>
+                    </div>
+
+                    <div class="operators">
+                        <div class="icon i-left" >
+                            <i></i>
+                        </div>
+                        <div class="icon i-left">
+                            <i class="icon-prev"></i>
+                        </div>
+                        <div class="icon i-center">
+                            <i></i>
+                        </div>
+                        <div class="icon i-right">
+                            <i class="icon-next"></i>
+                        </div>
+                        <div class="icon i-right">
+                            <i class="icon icon-not-favorite"></i>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="bottom">
-                
-                <div class="progress-wrapper">
-                    <span class="time time-l"></span>
-                    <div class="progress-bar-wrapper">
+        </transition>
+
+        <transition name="mini">
+
+            <div class="mini-player" v-show="!fullScreen" @click="open">
+
+                <div class="mini-player">
+                    <div class="icon">
+                        <img width="40" height="40" :src="currentSong.image">
+                    </div>
+
+                    <div class="text">
+                        <h2 class="name">{{currentSong.name}}</h2>
+                        <p class="desc">{{currentSong.singer}}</p>
+                    </div>
+
+                    <div class="control">
                         
                     </div>
-                    <span class="time time-r"></span>
-                </div>
 
-                <div class="operators">
-                    <div class="icon i-left" >
-                        <i></i>
-                    </div>
-                    <div class="icon i-left">
-                        <i class="icon-prev"></i>
-                    </div>
-                    <div class="icon i-center">
-                        <i></i>
-                    </div>
-                    <div class="icon i-right">
-                        <i class="icon-next"></i>
-                    </div>
-                    <div class="icon i-right">
-                        <i class="icon icon-not-favorite"></i>
+                    <div class="control">
+                        <i class="icon-playlist"></i>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="mini-player" v-show="!fullScreen" @click="open">
-            <div class="mini-player">
-                <div class="icon">
-                    <img width="40" height="40" :src="currentSong.image">
-                </div>
-
-                <div class="text">
-                    <h2 class="name">{{currentSong.name}}</h2>
-                    <p class="desc">{{currentSong.singer}}</p>
-                </div>
-
-                <div class="control">
-                    
-                </div>
-
-                <div class="control">
-                    <i class="icon-playlist"></i>
-                </div>
-            </div>
-        </div>
+        </transition>
         
     </div>
 </template>
 
 <script>
 import {mapGetters, mapMutations} from 'vuex';
+
+import animations from 'create-keyframe-animation';
 
 export default {
     name: 'Player',
@@ -91,6 +102,62 @@ export default {
         ])
     },
     methods: {
+        enter (el,done) {
+            const {x,y,scale} = this.getPosAndScale();
+
+            var animation = {
+                0: {
+                    transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+                },
+                60: {
+                    transform: `translate3d(0,0,0) scale(1.1)`
+                },
+                100: {
+                    transform: `translate3d(0,0,0) scale(1)`
+                }
+            }
+
+            animations.registerAnimation({
+                name: 'move',
+                animation,
+                presets: {
+                    duration: 400,
+                    easing: 'linear'
+                }
+            })
+
+            animations.runAnimation(this.$refs.cdWrapper, 'move', done);
+        },
+        afterEnter () {
+            animations.unregisterAnimation('move');
+            this.$refs.cdWrapper.style.animation = '';
+        },
+        leave (el,done) {
+            this.$refs.cdWrapper.style.transition = 'all .4s';
+            
+            const {x,y,scale} = this.getPosAndScale();
+
+            this.$refs.cdWrapper.style.transform = `translate3d(${x}px, ${y}px, 0) scale(${scale})`;
+
+            this.$refs.cdWrapper.addEventListener('transitionend', done);
+        },
+        afterLeave () {
+            this.$refs.cdWrapper.style.transition = '';
+            this.$refs.cdWrapper.style.transform = '';
+        },
+        getPosAndScale () {
+            const targetWidth = 40;
+            const paddingLeft = 40;
+            const paddingBottom = 30;
+            const paddingTop = 80;
+            const width = window.innerWidth * 0.8;
+            const scale = targetWidth / width;
+            const x     = -(window.innerWidth / 2 - paddingLeft) ;
+            const y     = window.innerHeight - paddingTop - width / 2- paddingBottom;
+            return {
+                x,y,scale
+            }
+        },
         open () {
             this.setFullScreen(true);
         },
