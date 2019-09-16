1548bb960affd8b1ae1831bbd56a5b527b2cfe5b
lhikarul <711hikaru@gmail.com>
2019-09-16 21:16:05 +0800

    7-23 歌詞左右滑動實現

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -17,7 +17,7 @@
                 </div>
 
                 <div class="middle" @touchstart.prevent="middleTouchStart" @touchmove.prevent="middleTouchMove" @touchend="middleTouchEnd">
-                    <div class="middle-l">
+                    <div class="middle-l" ref="middleL">
                         <div class="cd-wrapper" ref="cdWrapper">
                             <div class="cd" :class="cdCls">
                                 <img class="image" :src="currentSong.image">
@@ -312,15 +312,46 @@ export default {
             }
 
             const left  = this.currentShow === 'cd' ? 0 : -window.innerWidth;
-            const width = Math.min(0,Math.max(-window.innerWidth, left + deltaX)); 
-            
-            this.$refs.lyricList.$el.style.transform = `translate3d(${width}px,0,0)`;
+            const offsetWidth = Math.min(0,Math.max(-window.innerWidth, left + deltaX)); 
+            this.touch.percent = Math.abs(offsetWidth / window.innerWidth);
 
-
+            this.$refs.lyricList.$el.style.transform = `translate3d(${offsetWidth}px,0,0)`;
+            this.$refs.lyricList.$el.style.transitionDuration = 0;
+            this.$refs.middleL.style.opacity = 1 - this.touch.percent;
+            this.$refs.middleL.style.transitionDuration = 0;
 
         },
         middleTouchEnd (e) {
+            var offsetWidth;
+            var opacity;
 
+            if (this.currentShow === 'cd') {
+
+                if (this.touch.percent > 0.1) {
+                    offsetWidth = -window.innerWidth;
+                    opacity = 0;
+                    this.currentShow = 'lyric';
+                }else {
+                    offsetWidth = 0;
+                    opacity = 1;
+                }
+
+            }else {
+
+                if (this.touch.percent < 0.9) {
+                    offsetWidth = 0;
+                    this.currentShow = 'cd';
+                    opacity = 1;
+                }else {
+                    offsetWidth = -window.innerWidth;
+                    opacity = 0;
+                }
+            }
+
+            this.$refs.lyricList.$el.style.transform = `translate3d(${offsetWidth}px,0,0)`;
+            this.$refs.lyricList.$el.style.transitionDuration = 300 + 'ms';
+            this.$refs.middleL.style.opacity = opacity;
+            this.$refs.middleL.style.transitionDuration = 300 + 'ms';
         },
         pad (num,n = 2) {
             var len = num.toString().length;
