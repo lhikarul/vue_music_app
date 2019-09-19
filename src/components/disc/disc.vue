12c8364a909b775ce64209e8a66ae8f4ba5e8800
lhikarul <711hikaru@gmail.com>
2019-09-19 21:21:11 +0800

    8-3 歌單詳情頁 數據的應用

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <music-list :title="title" :bg-image="bgImage"></music-list>
+        <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
     </div>
 </template>
 
@@ -12,18 +12,39 @@ import {mapGetters} from 'vuex';
 import {ERR_OK} from 'api/config';
 import {getSongList} from 'api/recommend';
 
+import {createSong} from 'common/js/song';
+
 export default {
     components: {
         MusicList
     },
+    data () {
+        return {
+            songs: []
+        }
+    },
     methods: {
         _getSongList () {
-            console.log('wtf')
+
+            if (!this.dissid) {
+                this.$router.push('/recommend');
+                return;
+            }
+
             getSongList(this.disc.dissid).then((res) => {
                 if (res.code === ERR_OK) {
-                    console.log(res.cdlist[0].songlist)
+                    this.songs = this.normalizeSongs(res.cdlist[0].songlist);
                 }
             })
+        },
+        normalizeSongs (list) {
+            var ret = [];
+            list.forEach((musicData) => {
+                if (musicData.songid && musicData.albumid) {
+                    ret.push(createSong(musicData));
+                }
+            })
+            return ret;
         }
     },
     computed: {
