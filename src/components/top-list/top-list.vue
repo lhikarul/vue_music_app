f509e43d7a8672c4090d7405704663673768f1e9
lhikarul <711hikaru@gmail.com>
2019-09-23 15:16:20 +0800

    9-4 rank 詳情頁數據抓取與應用

--- src/components/top-list/top-list.vue
+++ src/components/top-list/top-list.vue
@@ -1,28 +1,67 @@
 <template>
     <div>
-        <music-list :title="title" :bg-image="bgImage"></music-list>
+        <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
     </div>
 </template>
 
 <script>
 import MusicList from 'components/music-list/musicList';
 import {mapGetters} from 'vuex';
+import {getMusicList} from 'api/rank';
+import {ERR_OK} from 'api/config';
+import {createSong} from 'common/js/song';
 
 export default {
     name: 'topList',
     components: {
         MusicList
     },
+    data () {
+        return {
+            songs: []
+        }
+    },
+    methods: {
+        _getMusicList () {
+
+            if (!this.topList.id) {
+                this.$router.push({
+                    path: '/rank'
+                })
+                return;
+            }
+
+            getMusicList(this.topList.id).then((res) => {
+                this.songs = this.normalizeSongs(res.songlist);
+            })
+        },
+        normalizeSongs (list) {
+            var ret = [];
+            list.forEach((item) => {
+                const musicData = item.data;
+                if (musicData.songid && musicData.albumid) {
+                    ret.push(createSong(musicData))
+                }
+            })
+            return ret;
+        }
+    },
     computed: {
         title () {
             return this.topList.topTitle;
         },
         bgImage () {
-            return this.topList.picUrl;
+            if (this.songs.length) {
+                return this.songs[0].image;
+            }
+            return ''
         },
         ...mapGetters([
             'topList'
         ])
+    },
+    created () {
+        this._getMusicList();
     }
 }
 </script>
